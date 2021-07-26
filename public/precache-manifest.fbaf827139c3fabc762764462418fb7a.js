9bbf3f3ca07ec3b81a09174d24e8011ba965f118
Wendy-M <wendymontagnon@gmail.com>
2021-07-26 16:09:18 +0200

    maj

---
+++ public/precache-manifest.fbaf827139c3fabc762764462418fb7a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e442688efce7f332cddc9ee12116cbee",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4fed1658630f7597a70c",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "3c0a28dd4e1f0c38437e",
+    "url": "/static/css/main.021aed33.chunk.css"
+  },
+  {
+    "revision": "4fed1658630f7597a70c",
+    "url": "/static/js/2.7f72b4bf.chunk.js"
+  },
+  {
+    "revision": "46a7fcc267e287f4c2c1c08c4574e640",
+    "url": "/static/js/2.7f72b4bf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3c0a28dd4e1f0c38437e",
+    "url": "/static/js/main.6ab1aea2.chunk.js"
+  },
+  {
+    "revision": "8d00fb4f0802f16afb92",
+    "url": "/static/js/runtime-main.5038b3ca.js"
+  }
+]);
--- public/service-worker 3.js
+++ public/service-worker 3.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.fbaf827139c3fabc762764462418fb7a.js"
+  "/precache-manifest.c5e5d63b9e442afac43557a4d5905547.js"
 );
 
 self.addEventListener('message', (event) => {
